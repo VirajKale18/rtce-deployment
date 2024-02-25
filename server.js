8b7f24d505a38ed19c2d63b6eb56ea1f98ef99b7
VirajKale18 <vnkthebeast@gmail.com>
2024-02-25 20:54:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+const ACTIONS = require('./src/Actions');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const userSocketMap = {};
+
+// Serve the frontend statically
+app.use(express.static(path.join(__dirname, 'build')));
+
+function getAllConnectedClients(roomId) {
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
+        return {
+            socketId,
+            username: userSocketMap[socketId],
+        };
+    });
+}
+
+io.on('connection', (socket) => {
+    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+        userSocketMap[socket.id] = username;
+        socket.join(roomId);
+        const clients = getAllConnectedClients(roomId);
+        clients.forEach(({ socketId }) => {
+            io.to(socketId).emit(ACTIONS.JOINED, {
+                clients,
+                username,
+                socketId: socket.id,
+            });
+        });
+    });
+
+    socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+    });
+
+    socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+    });
+
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+                socketId: socket.id,
+                username: userSocketMap[socket.id],
+            });
+        });
+        delete userSocketMap[socket.id];
+        socket.leave();
+    });
+});
+
+const PORT = process.env.PORT || 5005;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
